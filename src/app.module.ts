799c144f66043f66a466ef40273489c8822b339d
Oscar G <oscar.gtzv@hotmail.com>
2021-12-09 12:43:30 -0600

    added User entity and connected to db typeorm mysql

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WidgetController } from './widget/widget.controller';
+import { WidgetService } from './widget/widget.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/User'
+import { SocketController } from './socket/socket.controller';
+import { SocketService } from './socket/socket.service';
+import { ChatGateway } from './events.gateway';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '180496Mega-',
+    database: 'chatWidget',
+    entities: [User],
+    synchronize: true,
+  }),
+],
+  controllers: [AppController, WidgetController, SocketController],
+  providers: [AppService, WidgetService, SocketService, ChatGateway],
+})
+export class AppModule {}
