c31b6e2586c63a4ad47de5f274e7c2b787b0a238
Oscar G <oscar.gtzv@hotmail.com>
2021-12-15 20:42:37 -0600

    added websockets gateway

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -1,10 +1,24 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from 'socket.io';
+
+// const io = new Server({ /* options */ });
 
 @WebSocketGateway({ cors: true })
 export class ChatGateway {
+
+    @WebSocketServer()
+    server: Server;
+
     @SubscribeMessage('message')
-    handleEvent(@MessageBody() data: string): string {
-        console.log("message events gateway", data)
-        return data;
+    handleEvent(@MessageBody() data: string): void {
+        this.sendToAgent(data)
+        // return data;
     }
+
+    private sendToAgent(data) {
+        console.log('send to agent', data)
+        this.server.emit('agent', data)
+    }
+
+    
 }
