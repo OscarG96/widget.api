3251d4e373d704c7a94f7ec3997c7d60837a68e1
Oscar G <oscar.gtzv@hotmail.com>
2021-12-17 00:13:33 -0600

    added post request for testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World, from NestJs!';
+  }
+
+  createUser(user, email): any {
+    console.log(user, email)
   }
 }
