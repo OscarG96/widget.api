bc6288966316724aff8a3b39d5d60a10a2b978d4
Oscar G <oscar.gtzv@hotmail.com>
2021-12-17 00:13:48 -0600

    working with typeorm repository, has errors

--- src/widget/widget.service.ts
+++ src/widget/widget.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/User';
 
 @Injectable()
-export class WidgetService {}
+export class WidgetService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
+
+    createOne(user: any): Promise<User> {
+        return this.usersRepository.create(user)
+    }
+}
