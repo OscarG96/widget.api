6355f56419a3e3ce64f6b6ede1acd38af48dc51b
Oscar G <oscar.gtzv@hotmail.com>
2022-01-26 00:05:20 -0600

    removed Home and navbar component

--- widget.admin.web/src/App.js
+++ widget.admin.web/src/App.js
@@ -5,13 +5,12 @@ import NavbarWrapper from './components/navbar/NavbarWrapper';
 import { AuthProvider } from "./Auth"
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
-import Home from "./components/Home"
 import ChatWindow from './components/ChatWindow/ChatWindow';
 
 function App() {
   return (
     <div className="App">
-      <NavbarWrapper />
+      
       <AuthProvider>
         <Router>
           <PrivateRoute exact path="/" component={ChatWindow} />
